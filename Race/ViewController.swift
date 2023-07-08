aae50b25d6041056343096cb23d7636d9f54a73c
Никита Суровцев <nikitasurovcev@Nikitas-MacBook-Pro.local>
2023-07-09 00:51:57 +0300

    First commit

---
+++ Race/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Race
+//
+//  Created by Никита Суровцев on 9.07.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
